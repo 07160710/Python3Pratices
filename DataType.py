b5f2065da5529db1c78bf741df42a83a93160cc3
huangkaifeng <1195745286@qq.com>
2018-04-29 12:32:52 +0800

    首次提交

---
+++ DataType.py
@@ -0,0 +1,32 @@
+
+# 整型
+# 加
+print(25+25)
+# 减
+print(51-50)
+# 乘
+print(51*2)
+# 除
+print(153/51)
+print(155/51)
+# 地板除
+print(155//51)
+# 求余
+print(153%51)
+print(155%51)
+# 浮点型
+print(3.3*102)
+print(3.3*102+15.5)
+print(153/51.0)
+print(155/51.0)
+print(155//51.0)
+print(153/51.0)
+# 数据类型转换
+print(int(352.1))
+print(float(352.1))
+print(float(int(352.1)))
+
+# 常量
+# 变量
+xiaoming = "XIAOMING"
+print(xiaoming)
